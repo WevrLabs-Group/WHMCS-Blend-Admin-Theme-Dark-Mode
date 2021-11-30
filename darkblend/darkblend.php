773009d6d2b56622a2db619b99943db66b223d01
WevrLabs <wevrlabs@gmail.com>
2021-11-30 17:19:04 +0200

    Update darkblend.php

--- darkblend/darkblend.php
+++ darkblend/darkblend.php
@@ -14,11 +14,11 @@
 // *                                                                       *
 // *************************************************************************/
 
-use WHMCS\Database\Capsule;
-
 if (!defined("WHMCS")) {
     die("This file cannot be accessed directly");
 }
+
+use WHMCS\Database\Capsule;
 
 function darkblend_config()
 {
@@ -26,7 +26,7 @@ function darkblend_config()
 
     return [
         'name'        => 'Blend Dark Mode',
-        'description' => 'This module adds a dark mode for WHMCS admin theme Blend. To enable the dark UI, simply Activate the addon, and to disable the dark mode, simple Deactivate the addon.',
+        'description' => 'This module adds a dark mode for WHMCS admin theme Blend. To enable the dark UI, simply Activate the addon, and to disable the dark mode, Deactivate the addon.',
         'author'      => "<a href='https://wevrlabs.net' target='_blank' title='contributed by WevrLabs Hosting'><img style='padding:7px;width:150px' src='$logo' alt='contributed by WevrLabs Hosting'></a>",
         'language'    => 'english',
         'version'     => '3.0.0',
