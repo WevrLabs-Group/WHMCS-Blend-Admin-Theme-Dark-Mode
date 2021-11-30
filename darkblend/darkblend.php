56f5d89c0f4861282937a2a3b05b5d2224c6f7fd
WevrLabs <wevrlabs@gmail.com>
2021-11-30 17:20:23 +0200

    version number

--- darkblend/darkblend.php
+++ darkblend/darkblend.php
@@ -29,6 +29,6 @@ function darkblend_config()
         'description' => 'This module adds a dark mode for WHMCS admin theme Blend. To enable the dark UI, simply Activate the addon, and to disable the dark mode, Deactivate the addon.',
         'author'      => "<a href='https://wevrlabs.net' target='_blank' title='contributed by WevrLabs Hosting'><img style='padding:7px;width:150px' src='$logo' alt='contributed by WevrLabs Hosting'></a>",
         'language'    => 'english',
-        'version'     => '3.0.0',
+        'version'     => '3.0.3',
     ];
 }
